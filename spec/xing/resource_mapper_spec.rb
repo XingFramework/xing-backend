1ef173f920c4b2af8fd279b60c8e46d571a254bf
Evan Dorn <evan@lrdesign.com>
2015-06-11 15:20:53 -0700

    First tests for a Xing base class.

---
+++ spec/xing/resource_mapper_spec.rb
@@ -0,0 +1,56 @@
+require 'xing/resource_mapper'
+
+describe Xing::ResourceMapper do
+  it "should exist" do
+    Xing::ResourceMapper.should be_a(Module)
+  end
+
+  context "subclasses" do
+    let :record do
+      double("some record").tap do |obj|
+        allow(obj).to receive(:save).and_return(true)
+      end
+    end
+    let :json do
+      { 'links'=> {},
+        'data'=> { some: "value"}
+      }
+    end
+
+    before do
+      stub_const("ActiveModelErrorConverter", double('error converter')).tap do |stub|
+        allow(stub).to receive_message_chain(:new, :convert).and_return({})
+      end
+    end
+
+    it "should error if it doesn't have an update_record method and a record_class" do
+      class MyMapper < Xing::ResourceMapper
+      end
+
+      mapper = MyMapper.new(json)
+      mapper.record = record
+
+      expect do
+        mapper.save
+      end.to raise_error
+    end
+
+    it "shouldn't error if it does have an update_record and a record_class method" do
+      class MyBetterMapper < Xing::ResourceMapper
+        def update_record
+        end
+        def record_class
+          Object
+        end
+      end
+
+      mapper = MyBetterMapper.new(json)
+      mapper.record = record
+
+      expect do
+        mapper.save
+      end.not_to raise_error
+    end
+  end
+
+end
