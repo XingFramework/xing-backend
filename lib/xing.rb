b65e54eb063d7b5c020f122d002c9413546aabfd
Evan Dorn <evan@lrdesign.com>
2015-06-12 03:35:30 -0700

    Break out the rails engine spec to a different file.

--- lib/xing.rb
+++ lib/xing.rb
@@ -3,26 +3,6 @@ require 'rails'
 module Xing
   mattr_accessor :backend_subdomain
 
-  class Engine < ::Rails::Engine
-    isolate_namespace Xing
-
-    config.generators do |g|
-      g.test_framework :rspec
-    end
-
-    initializer 'xing errors locales path' do
-      I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '*.{rb,yml}')]
-    end
-
-    initializer 'set subdomain' do
-      # Set the backend subdomain if it hasn't been configured by the user.
-      Xing.configure do |xng_config|
-        xng_config.backend_subdomain ||= 'api'
-      end
-    end
-
-  end
-
   # Configure xing via pattern similar to Rails:
   #
   # Xing.configure do |config|
@@ -36,6 +16,7 @@ module Xing
   end
 end
 
+require 'xing/engine'
 require 'xing/mappers'
 require 'xing/serializers'
 require 'xing/services'
