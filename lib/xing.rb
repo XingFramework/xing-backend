2ab89f18c593895d48923d161ea1c56518b53cd4
Evan Dorn <evan@lrdesign.com>
2015-06-11 17:58:43 -0700

    clean up railtie a bit

--- lib/xing.rb
+++ lib/xing.rb
@@ -4,11 +4,11 @@ module Xing
 
   class Railtie < Rails::Railtie
     initializer 'xing autoload', :before => :set_autoload_paths do |app|
-      # Include the deprecated old constants
-      config.autoload_paths += Dir[ Rails.root.join("lib", "framework", "**")]
 
-      # this line for the new, namespaced framework files
-      config.autoload_paths += Dir[ Rails.root.join("lib", "framework", "xing","**")]
+
+      # Commented out pending discovery if we actually need this config.
+      #
+      #config.autoload_paths += Dir[ Rails.root.join("lib", "framework", "xing","**")]
     end
   end
 end
