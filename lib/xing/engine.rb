c01304781e6f51771f2a0f1cd25a2d52b9a725cc
Evan Dorn <evan@lrdesign.com>
2015-06-12 03:36:58 -0700

    Break out the rails engine spec to a different file.  Remove some unnecessary files.

---
+++ lib/xing/engine.rb
@@ -0,0 +1,25 @@
+module Xing
+  class Engine < ::Rails::Engine
+    isolate_namespace Xing
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    # The ErrorConverter leverages (abuses?) the I18n mechanism to translate
+    # ActiveModel validation errors into Xing JSON resource errors.  Here we
+    # need to make sure the locales file for language 'json' is loaded for
+    # I18n.
+    initializer 'xing errors locales path' do
+      I18n.load_path += Dir[File.join(File.dirname(__FILE__), '..', 'config', 'locales', '*.{rb,yml}')]
+    end
+
+    # Set the backend subdomain if it hasn't been configured by the user.
+    initializer 'set subdomain' do
+      Xing.configure do |xng_config|
+        xng_config.backend_subdomain ||= 'api'
+      end
+    end
+
+  end
+end
