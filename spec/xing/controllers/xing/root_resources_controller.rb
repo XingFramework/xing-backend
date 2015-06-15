1a80f897601dfe1b63ecdced4f6560a2ddf21ab9
Evan Dorn <evan@lrdesign.com>
2015-06-15 09:48:05 -0700

    Fix file structure for controllers. Remove a deprecated call from spec_helper.

--- spec/xing/controllers/xing/root_resources_controller.rb
+++
@@ -1,8 +0,0 @@
-module Xing
-  class RootResourcesController < ::Xing::BaseController
-    def index
-      @resources = rfc6570_routes(ignore: %w(format), path_only: true)
-      render :json => Xing::Serializer::RootResources.new(@resources)
-    end
-  end
-end
--- spec_help/spec_helper.rb
+++ spec_help/spec_helper.rb
@@ -9,7 +9,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun'
 
 ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 Dir[File.join(ENGINE_RAILS_ROOT, "spec_help/support/**/*.rb")].each {|f| require f }
