d3e7085ba5af97cc9b614281cf2540ce27d3e9e2
Judson <nyarly@gmail.com>
2015-07-23 15:20:49 -0700

    Pulling out deprecated classes checks

--- spec/deprecated_classes/remote_snapshot_fetcher_spec.rb
+++
@@ -1,8 +0,0 @@
-
-require 'deprecated_classes'
-
-describe RemoteSnapshotFetcher do
-  it "should be the correct class" do
-    expect(RemoteSnapshotFetcher.new()).to be_a(Xing::Services::SnapshotFetcher)
-  end
-end
